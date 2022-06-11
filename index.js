f0f5a32f7da10d73eb6e75bde6ec68f43dd5047a
Marlee Garcia <marlee.garcia121@gmail.com>
2022-06-11 18:17:00 -0400

    I don't know how git ignore works but this is task two

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const {numbers, name, hometown} = require("./data");
 const sum = require("./sum");
+const cowsay = require("cowsay");
+const lodash = require("lodash");
 
 console.log(numbers);
 console.log(`${name} is from ${hometown}`);
 
 console.log(sum(numbers));
+
+console.log(cowsay.say({
+    text: "Hello world!",
+    e: "uu",
+    T: " u"
+}));
+
+console.log(lodash.shuffle(numbers));
